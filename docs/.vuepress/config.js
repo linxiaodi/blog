adca875e7b068cc9eb30c84cef13307073d7c8a8
林伟琨 <375690260@qq.com>
2020-05-25 09:48:43 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: '盗梦空间',
+  description: '林伟琨的写字台',
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    nav: require('./nav'),
+  }
+}
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    text: '博客',
+    link: '/blog/'
+  },
+  {
+    text: 'Java',
+    link: '/java/'
+  },
+  { text: 'Github', link: 'https://github.com/linxiaodi/blog', target:'_blank' }
+ 
+]
